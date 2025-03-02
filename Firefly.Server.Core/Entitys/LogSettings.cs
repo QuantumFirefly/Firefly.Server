aa114f4a5cee9cb3e36e2ca57da67f1e956a3882
Lee Dalchow <lsdalchow@gmail.com>
2025-03-02 00:58:29 +0000

    Added nlog.config to Core & Fleshed out LogSettings.cs

--- Firefly.Server.Core/Entitys/LogSettings.cs
+++ Firefly.Server.Core/Entitys/LogSettings.cs
@@ -1,18 +1,36 @@
 ﻿using Firefly.Server.Core.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using NLog;
 
 namespace Firefly.Server.Core.Entitys;
     public class LogSettings : ISettings
 {
+
+    public string LogLevel { get; set; } // TODO Enum. None, INFO, DEBUG, TRACE, I think I'm missing one?
+    public string logFilePath { get; set; } // Default: FireflyServer_${shortdate}.log
+
+    public string logArchivePath { get; set; } // Default: FireflyServer_LogArchive.{#}.zip // TODO: Check variables. Can we have a date here?
+
+    public string archiveEvery { get; set; } // Default: Day - Can this be an Enum?
+
+    public string archiveNumbering { get; set; } // Default: Rolling - Can this be an Enum?
+
+    public int maxArchiveFiles { get; set; } // Default: 10
+
     public bool Validate(ref List<string> messages)
     {
         bool validationPassed = true;
 
+        // TODO - Validation
 
         return validationPassed;
+    }
+
+    public void ApplySettingsToNLog()
+    {
+        GlobalDiagnosticsContext.Set("logFilePath", logFilePath);
+        GlobalDiagnosticsContext.Set("logArchivePath", logArchivePath);
+        GlobalDiagnosticsContext.Set("archiveEvery", archiveEvery);
+        GlobalDiagnosticsContext.Set("archiveNumbering", archiveNumbering);
+        GlobalDiagnosticsContext.Set("maxArchiveFiles", maxArchiveFiles);
     }
 }
