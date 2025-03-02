fcc3bb1d429194eac45887f2ad7e00898d33a81e
Lee Dalchow <lsdalchow@gmail.com>
2025-03-02 21:47:23 +0000

    Refactored Local Settings Import & Validation to private method and added todo for future dev.

--- Firefly.Server.Worker/MasterWorker.cs
+++ Firefly.Server.Worker/MasterWorker.cs
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
 
 namespace Firefly.Server.Worker
 {
-    
+
 
     internal class MasterWorker
     {
@@ -30,13 +30,34 @@ namespace Firefly.Server.Worker
             Console.WriteLine($"Firefly Server v{version} Booting up...");
 
             Console.WriteLine($"Reading {Constants.LOCAL_SETTINGS_INI_FILE}...");
+
             LocalSettings localSettings;
+            if (!_importValidateAndApplyLocalSettings(out localSettings, Constants.LOCAL_SETTINGS_INI_FILE, Constants.ENVIRONMENT_TYPE)) {
+                return;
+            }
+
+            _log.Log(LogLevel.Info, $"Firefly Server v{version} - Local Settings Imported & Validated.");
+            
+            /* TODO
+             * Connect to Database. Test can run some querys
+             * Upgrade to latest version if needed (Creating tables, alter statements, etc). Is there a library I can use to do this?
+             * Import RemoteSettings from database table (need an appropiate name, FireFlySettings? Probably better to have a parent calss also that has both local & remote settings)
+             * Create IRC Settings - and class hierachy for it.
+             * Start to listen in on IRC port for inbound TCP connections. Start new worker thread for each connection.
+             * 
+             */
+
+
+        }
+
+        private bool _importValidateAndApplyLocalSettings(out LocalSettings localSettings, string iniFile, string dbEnvironment)
+        {
             try
             {
-                localSettings = new LocalSettingsFactory(Constants.LOCAL_SETTINGS_INI_FILE, Constants.ENVIRONMENT_TYPE).Build();
+                localSettings = new LocalSettingsFactory(iniFile, dbEnvironment).Build();
 
                 var messages = new List<String>();
-                if(!localSettings.Validate(ref messages))
+                if (!localSettings.Validate(ref messages))
                 {
                     var exMsg = string.Join("; ", messages);
                     throw new Exception(exMsg);
@@ -44,20 +65,23 @@ namespace Firefly.Server.Worker
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"ERROR: Unable to read from {Constants.LOCAL_SETTINGS_INI_FILE}. {ex.Message}.");
-                return;
+                Console.WriteLine($"ERROR: Unable to read from {iniFile}. {ex.ToString()}.");
+                localSettings = null;
+                return false;
             }
 
-            localSettings.LogSettings.ApplySettingsToNLog();
-
-
-            /*while(true)
+            try
             {
-                _log.Log(LogLevel.Info, "blah blah");
-            }*/
-                
+                localSettings.LogSettings.ApplySettingsToNLog();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ERROR: Error applying config to NLog. {ex.ToString()}.");
+                localSettings = null;
+                return false;
+            }
             
+            return true;
+        }
     }
-
-}
 }
