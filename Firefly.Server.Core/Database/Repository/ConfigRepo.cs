4b3cb66495068607ff01921016529aa30aa37e1d
Lee Dalchow <lsdalchow@gmail.com>
2025-03-05 23:37:33 +0000

    Began to build out database structure and Config Entity

--- Firefly.Server.Core/Database/Repository/ConfigRepo.cs
+++ Firefly.Server.Core/Database/Repository/ConfigRepo.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Firefly.Server.Core.Entitys;
 
 namespace Firefly.Server.Core.Database.Repository
 {
-    internal class ConfigRepo(DbConnection db)
+    public class ConfigRepo(DbConnection db)
     {
         private readonly DbConnection _db;
 
+        public Config GetAll() {
+            
+            // Call DB. Get Query from Query Area
+        }
 
         // TODO - GetAll(), Get(), SetAll(), Set()
         //  Deserialises specific fields.
--- Firefly.Server.Core/Entitys/Config.cs
+++ Firefly.Server.Core/Entitys/Config.cs
@@ -9,20 +9,19 @@ namespace Firefly.Server.Core.Entitys
     public class Config
     {
 
+        public GeneralConfig General {  get; set; }
+        public struct GeneralConfig() {
+            public string ServerName { get; set; }
+            public string ServerDomain { get; set; }
+        }
 
-        /*
-         * TODO - Proposed config structure in DB.
-         * 
-         * 1) Table: id, key, value. value to be JSONB data-type
-         * 2) Config settings to be hierachical.
-         * 3) Config.cs Entity to match top-level table. Lower down to have object hierarchy for the config. JSON structure. Top-level stored within Config.cs Anything unknown goes into a Dictionary Key->Value.
-         * 4) Example: (Something like this:)
-         * 5) IRC -> Enabled, Port (This would have an IRCConfig class that contained a bool Enabled and int Port). Upper level Config table would have IRC (Key) -> (Value) "Enabled": True, "Port": 6667
-         * 6) General -> domain
-         * 7) Authentication -> allowAnonUsers
-         * 
-         * Concern: Is there a better way to store this than some complex class structure? I foresee this growing large, perhaps small classes stored within a single file is a good way to store this.
-         * would record, struct or record struct be more appropiate than a class?
-         */
+
+
+        public IRCConfig IRC = new IRCConfig();
+        public struct IRCConfig
+        {
+            public int Port { get; set; }
+            public bool Enabled { get; set; }
+        }
     }
 }
